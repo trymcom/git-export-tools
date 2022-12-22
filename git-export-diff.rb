d107b51781ef9d7716c96efebaf100ccfeefeb14
Miura Keisuke <miura@trym.jp>
2022-12-22 14:08:21 +0900

    [update] 社内wikiの記述を反映

--- git-export-diff.rb
+++ git-export-diff.rb
@@ -10,6 +10,7 @@
 #
 # If you want to use with SourceTree, set custom action with argument $SHA
 #
+Encoding.default_external = Encoding::UTF_8
 require "yaml"
 require "optparse"
 require 'shellwords'
@@ -78,7 +79,7 @@ else
   desc = %x[git describe --contains --all #{sha}]
   name = case desc
     when /^tags/
-      desc[/tags\/([\w\/]+)\^0/, 1].strip
+      desc[/tags\/(\w+)\^0/, 1].strip
     when /^remotes/
       desc[/remotes\/(\w+\/\w+)/, 1].gsub("/", "-").strip + "-#{sha[0,7]}"
     else
